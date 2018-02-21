7ac6715c44b256c855189744e5379e48f027ffd3
Ray Suyoto <ray.suy@icloud.com>
2018-02-20 17:28:49 -0700

    js buttons fixed

--- routes/cars.js
+++ routes/cars.js
@@ -1,21 +1,51 @@
-var express = require('express');
-var router = express.Router();
-var Cars = require('../models').Cars;
+var express = require('express')
+var router = express.Router()
+var Car = require('../cars').Cars
 
-/* GET car listings. */
-router.get('/', function(req, res) {
-  Cars.all()
-    .then( function(cars) {
-      return res.render('cars', { cars: cars });
-  })
-});
-
-/* POST add car listing */
-router.post('/', function(req, res) {
-  var make = req.body.make;
-  Car.create({ make: make })
+// PUT /movies/7
+router.put('/:id', function(req, res) {
+  Car.update(
+    { title: req.body.title },
+    { where: { id: req.params.id }}
+  )
     .then( function() {
-      res.redirect('/cars');
-  });
-});
+      return res.redirect('/cars')
+    })
+})
+
+// GET /movies/7/edit
+router.get('/:id/edit', function(req, res) {
+  Car.findById(req.params.id)
+    .then( function(car) {
+      return res.render('edit', { car: car })
+    })
+})
+
+//  DELETE /movies/7
+router.delete('/:id', function(req, res) {
+  Car.findById(req.params.id)
+    .then( function(car) { car.destroy() })
+    .then( function() { return res.redirect('/cars') })
+  
+})
+
+// GET /movies
+router.get('/', function(req, res) {
+  Car.all({
+    order: [['createdAt', 'ASC']]
+  })
+    .then( function(cars) {
+      res.render('cars', { cars: cars })
+    })
+})
+
+// POST /movies
+router.post('/', function(req, res) {
+  var title = req.body.title
+  Car.create({ title: title })
+    .then( function() {
+      res.redirect('/cars')
+    })
+})
+
 module.exports = router;
